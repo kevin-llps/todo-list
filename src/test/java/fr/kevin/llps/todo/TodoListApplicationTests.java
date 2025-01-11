827f8482b8f55c0b0aafb7e9494454a0d74f036b
kevin-llps <llopis.kevin@outlook.fr>
2025-01-11 15:56:27 +0100

    feat: Init project

---
+++ src/test/java/fr/kevin/llps/todo/TodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.kevin.llps.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
