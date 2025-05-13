84b55ad52a85451a0475642781b8bf3b10f70c4b
kevin-llps <llopis.kevin@outlook.fr>
2025-05-13 15:56:24 +0200

    feat: Remove optional Todo fields

--- src/main/java/fr/kevin/llps/todo/entity/Todo.java
+++ src/main/java/fr/kevin/llps/todo/entity/Todo.java
@@ -1,16 +1,9 @@
 package fr.kevin.llps.todo.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
@@ -29,11 +22,5 @@ public class Todo {
 
     @Column(name = "completed", nullable = false, columnDefinition = "BOOLEAN")
     private Boolean completed;
-
-    @Column(name = "num_order", nullable = true, columnDefinition = "INT")
-    private Integer numOrder;
-
-    @Column(name = "expiry_date", nullable = true, columnDefinition = "DATETIME")
-    private LocalDateTime expiryDate;
 
 }
--- src/main/java/fr/kevin/llps/todo/exception/TodoNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package fr.kevin.llps.todo.exception;
-
-public class TodoNotFoundException extends RuntimeException{
-
-    public TodoNotFoundException(String message) {
-        super(message);
-    }
-
-}
