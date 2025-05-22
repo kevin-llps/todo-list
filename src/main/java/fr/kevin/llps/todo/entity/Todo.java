33c2541e6f3550de0522b52ad9ee0b42d32b107e
kevin-llps <llopis.kevin@outlook.fr>
2025-05-22 11:15:30 +0200

    Revert "feat: Remove optional Todo fields"
    This reverts commit 84b55ad52a85451a0475642781b8bf3b10f70c4b.

--- src/main/java/fr/kevin/llps/todo/entity/Todo.java
+++ src/main/java/fr/kevin/llps/todo/entity/Todo.java
@@ -1,9 +1,16 @@
 package fr.kevin.llps.todo.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
@@ -22,5 +29,11 @@ public class Todo {
 
     @Column(name = "completed", nullable = false, columnDefinition = "BOOLEAN")
     private Boolean completed;
+
+    @Column(name = "num_order", nullable = true, columnDefinition = "INT")
+    private Integer numOrder;
+
+    @Column(name = "expiry_date", nullable = true, columnDefinition = "DATETIME")
+    private LocalDateTime expiryDate;
 
 }
